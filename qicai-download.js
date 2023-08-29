537255f033b291fca0db41d6ade74402888b6320
Miranda Roberts <32008882+RongLiXu@users.noreply.github.com>
2023-08-29 17:02:40 +0800

    Create qicai-download.js

---
+++ qicai-download.js
@@ -0,0 +1,16 @@
+callback([{
+    "url": "https://wwqf.lanzoue.com/iQnQg16tme3g",
+    "name": "Download for MacOS",
+    "article": "Requires macOS Catalina 10.15 or later.",
+    "password": ""
+}, {
+    "url": "https://wwqf.lanzoue.com/i7seX16tmfaj",
+    "name": "Download for Windows",
+    "article": "Requires Windows 8 or later.",
+    "password": ""
+}, {
+    "url": "https://wwqf.lanzoue.com/iCkVz16tmfdc",
+    "name": "Download for Android",
+    "article": "Requires Android 6.0 or later.",
+    "password": "123456"
+}])
